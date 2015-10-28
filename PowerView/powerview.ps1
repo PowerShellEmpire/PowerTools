2640d0408b902aa3743fb40c208b3db3dd0691c2
Harmj0y <will@harmj0y.net>
2015-10-28 15:58:20 -0400

    Added Invoke-ACLScanner

--- PowerView/powerview.ps1
+++ PowerView/powerview.ps1
@@ -2,7 +2,7 @@
 
 <#
 
-    PowerView v2.0.1
+    PowerView v2.0.2
 
     See README.md for more information.
 
@@ -2451,9 +2451,19 @@ function Get-ObjectAcl {
                 $Searcher.FindAll() | Foreach-Object {
                     $Object = [adsi]($_.path)
                     $Access = $Object.PsBase.ObjectSecurity.access
-                    # add in the object DistinguishedName to the output object
-                    $Access | Add-Member NoteProperty 'ObjectDN' ($_.properties.distinguishedname[0])
-                    $Access
+                    $Access | ForEach-Object {
+                        $_ | Add-Member NoteProperty 'ObjectDN' ($Object.distinguishedname[0])
+
+                        if($Object.objectsid[0]){
+                            $S = (New-Object System.Security.Principal.SecurityIdentifier($Object.objectsid[0],0)).Value
+                        }
+                        else {
+                            $S = $Null
+                        }
+                        
+                        $_ | Add-Member NoteProperty 'ObjectSID' $S
+                        $_
+                    }
                 } | ForEach-Object {
                     if($RightsFilter) {
                         $GuidFilter = Switch ($RightsFilter) {
@@ -2478,9 +2488,6 @@ function Get-ObjectAcl {
                                 catch {
                                     $AclProperties[$_.Name] = $_.Value
                                 }
-                            }
-                            elseif(($_.Name -eq 'IdentityReference') -and ($_.Value -match '^S-1-5-*')) {
-                                $AclProperties[$_.Name] = Convert-SidToName $_.Value
                             }
                             else {
                                 $AclProperties[$_.Name] = $_.Value
@@ -2721,6 +2728,117 @@ function Add-ObjectAcl {
                 Write-Warning "Error: $_"
             }
         }
+    }
+}
+
+
+function Invoke-ACLScanner {
+<#
+    .SYNOPSIS
+        Searches for ACLs for specifable AD objects (default to all domain objects)
+        with a domain sid of > -1000, and have modifiable rights.
+
+        Thanks Sean Metcalf (@pyrotek3) for the idea and guidance.
+
+    .PARAMETER SamAccountName
+
+        Object name to filter for.        
+
+    .PARAMETER Name
+
+        Object name to filter for.
+
+    .PARAMETER DistinguishedName
+
+        Object distinguished name to filter for.
+
+    .PARAMETER Filter
+
+        A customized ldap filter string to use, e.g. "(description=*admin*)"
+     
+    .PARAMETER ADSpath
+
+        The LDAP source to search through, e.g. "LDAP://OU=secret,DC=testlab,DC=local"
+        Useful for OU queries.
+
+    .PARAMETER ADSprefix
+
+        Prefix to set for the searcher (like "CN=Sites,CN=Configuration")
+
+    .PARAMETER Domain
+
+        The domain to use for the query, defaults to the current domain.
+
+    .PARAMETER DomainController
+
+        Domain controller to reflect LDAP queries through.
+
+    .PARAMETER ResolveGUIDs
+
+        Switch. Resolve GUIDs to their display names.
+
+    .PARAMETER PageSize
+
+        The PageSize to set for the LDAP searcher object.
+
+    .EXAMPLE
+
+        PS C:\> Invoke-ACLScanner -ResolveGUIDs | Export-CSV -NoTypeInformation acls.csv
+
+        Enumerate all modifable ACLs in the current domain, resolving GUIDs to display 
+        names, and export everything to a .csv
+#>
+
+    [CmdletBinding()]
+    Param (
+        [Parameter(ValueFromPipeline=$True)]
+        [String]
+        $SamAccountName,
+
+        [String]
+        $Name = "*",
+
+        [Alias('DN')]
+        [String]
+        $DistinguishedName = "*",
+
+        [String]
+        $Filter,
+
+        [String]
+        $ADSpath,
+
+        [String]
+        $ADSprefix,
+
+        [String]
+        $Domain,
+
+        [String]
+        $DomainController,
+
+        [Switch]
+        $ResolveGUIDs,
+
+        [ValidateRange(1,10000)] 
+        [Int]
+        $PageSize = 200
+    )
+
+    # Get all domain ACLs with the appropriate parameters
+    Get-ObjectACL @PSBoundParameters | ForEach-Object {
+        # add in the translated SID for the object identity
+        $_ | Add-Member Noteproperty 'IdentitySID' ($_.IdentityReference.Translate([System.Security.Principal.SecurityIdentifier]).Value)
+        $_
+    } | Where-Object {
+        # check for any ACLs with SIDs > -1000
+        try {
+            [int]($_.IdentitySid.split("-")[-1]) -ge 1000
+        }
+        catch {}
+    } | Where-Object {
+        # filter for modifiable rights
+        ($_.ActiveDirectoryRights -eq "GenericAll") -or ($_.ActiveDirectoryRights -match "Write") -or ($_.ActiveDirectoryRights -match "Create") -or ($_.ActiveDirectoryRights -match "Delete") -or (($_.ActiveDirectoryRights -match "ExtendedRight") -and ($_.AccessControlType -eq "Allow"))
     }
 }
 
